ca240fb3ea1c753a0f4dbf4d031c647c086bc0b5
Gilang Erlianul Awal <144137514+excaliburXD@users.noreply.github.com>
2024-05-06 10:56:03 +0700

    fix typo

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-COMMON_PATH := device/transsion/mt6789-common
+COMMON_PATH := device/transsion/mt6785-common
 
 TARGET_RECOVERY_DEVICE_DIRS := \
     $(COMMON_PATH) \
